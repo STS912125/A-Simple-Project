1f9531d8c3e25a2facccb8d7001e763d2d6dc925
STS12125 <mymobileacount666@gmail.com>
2025-01-25 16:04:36 +0000

    Uploaded

---
+++ Solution using R Script/2021-22 Question 1(D).R
@@ -0,0 +1,14 @@
+#Problem I am trying to solve:
+
+#2021-22 Q1(D):
+
+#An automobile manufacturer is concerned about a possible recall of its bestselling
+#four-door sedan. If there were a recall, there is a probability of 0.25 of a defect
+#in the brake system, 0.18 of a defect in the transmission, 0.17 of a defect in the fuel
+#system, and 0.40 of a defect in some other area.
+#(i) What is the probability that the defect is the brakes or the fueling system if the
+#probability of defects in both systems simultaneously is 0.I5?
+#(ii) What is the probability that there are no defects in either the brakes or the
+#fueling system?
+
+
