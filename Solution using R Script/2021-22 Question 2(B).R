34fd904f5211fdb5156048dca74a666ca9a1b886
STS912125 <mymobileacount666@gmail.com>
2025-01-26 07:14:19 +0600

    New Problem Uploaded

---
+++ Solution using R Script/2021-22 Question 2(B).R
@@ -0,0 +1,8 @@
+#Problem I am trying to solve:
+
+#2021-22 Q2(B):
+
+# A service facility operates with two service lines. On a randomly selected day, let
+#X be the proportion of time that the first line is in use whereas Y is the proportion of
+#time that the second line is in use. Suppose that the joint probability density function
+#for (X Y) is
