c51c804ce1179b884fadd13aae43039d1fadb4e3
STS912125 <mymobileacount666@gmail.com>
2025-02-02 07:23:08 +0600

    Uploaded

--- Solution using R Script/2021-22 Question 5(C).R
+++ Solution using R Script/2021-22 Question 5(C).R
@@ -5,3 +5,6 @@
 #(c) Let X1 and X2 be independent random variables each having the probability distribution:
 #f(x)={e^(−x),when x>0, 0 elsewhere}
 #Show that the random variables Y1 and Y2 are independent when Y1 = X1 + X2 and Y2 = X1/( X1 + X2)
+
+P(x1,x2) = e^(-(x1,x2))
+y1=x1+x2
